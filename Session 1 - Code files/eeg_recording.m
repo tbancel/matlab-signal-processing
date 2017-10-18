a0159bf6c91950d35232395667e9a5cc8fba519c
Thomas Bancel <bancel.thomas@gmail.com>
2017-10-18 18:17:49 +0200

    Add windowing

--- Session 1 - Code files/eeg_recording.m
+++ Session 1 - Code files/eeg_recording.m
@@ -3,12 +3,12 @@ clear;
 close;
 clc;
 
-load('/Users/tbancel/Desktop/MS_BME/UE 3.6-7 BCI/matlab-signal-processing/artefact_1.mat')
+load('artefact_1.mat')
 data = s_EEG.data;
 Fp1=data(1,:)
-plot(Fp1);
+% plot(Fp1);
 
-sample_length=length(Fp1)
+sample_length=length(Fp1);
 epoch_number = 20;
 epoch_length=sample_length/epoch_number;
 epochs=[];
@@ -26,6 +26,13 @@ epochs_label=[]
 
 % plotting the epochs with the right color
 figure
+
+subplot(2,1,1)
+plot(Fp1);
+title("EEG recording")
+ylabel("mV")
+
+subplot(2,1,2)
 for i=0:(epoch_number-1),
     if max(abs(epochs(i+1,:)))>40,
         epochs_label(i+1)=0;
@@ -35,7 +42,34 @@ for i=0:(epoch_number-1),
         color = 'b';
     end
     X=(i*epoch_length+1):((i+1)*epoch_length);
-    plot(X,epochs(i+1,:), color), axis([0 30620 -60 100]);
+    plot(X,epochs(i+1,:), color), axis([0 35000 -100 150]);
     hold on
 end   
+title("EEG recording, threshold = 40mV")
+ylabel("mV")
+
+%% hamming windowing
+
+h_window=hamming(epoch_length);
+epochs_windowed = [];
+diff = [];
+
+figure
+
+subplot(2,1,1)
+plot(Fp1);
+title("EEG recording")
+ylabel("mV")
+
+subplot(2,1,2)
+for i=0:(epoch_number-1),
+    epochs_windowed(i+1,:)=epochs(i+1,:).*transpose(h_window);
+    diff(i+1,:)=epochs(i+1,:)-epochs_windowed(i+1,:);
+    X=(i*epoch_length+1):((i+1)*epoch_length);
+    plot(X,epochs_windowed(i+1,:)), axis([0 35000 -100 150]);
+    hold on
+end   
+title("Epoched signal")
+ylabel("mV")
+
 
